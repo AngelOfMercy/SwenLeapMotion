5f5c041a20994aa2940a33aa7e19e1104efeaf3f
Alistair Eichler <alistair.eichler.ask@gmail.com>
2016-08-18 11:12:41 +1200

    Main now runs the application properly

--- src/main.java
+++ src/main.java
@@ -1,4 +1,25 @@
+import java.io.IOException;
+
+import Leap.LeapController;
+import display.Display;
+import world.World;
 
 public class main {
+	
+	public static void main(String args[]){
+		World world = new World();
+		Display display = new Display(world);
+		LeapController lc = new LeapController(world, display);
+	
+		display.addListener(lc);
+	
+	try{
+		System.in.read();
+	}
+	catch(IOException e){
+		e.printStackTrace();
+	}
 
+		display.removeListener(lc);
+	}
 }
